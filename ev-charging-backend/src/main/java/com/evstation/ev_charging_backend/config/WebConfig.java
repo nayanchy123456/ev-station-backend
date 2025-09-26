9f4afe196139fe347d3cc37985ef2cffb8a35c1a
nayanchy123456 <nayanchy2004@gmail.com>
2025-09-26 11:24:37 +0545

    WebConfig is added in oder to integrate the endpoint with the frontend

---
+++ ev-charging-backend/src/main/java/com/evstation/ev_charging_backend/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.evstation.ev_charging_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // allow all endpoints
+                        .allowedOrigins("http://localhost:5173") // your frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // allowed HTTP methods
+                        .allowedHeaders("*"); // allow all headers
+            }
+        };
+    }
+}
